735adf4e6fd431c60867f3396dd7b607542ec2f3
Nestor Alfaro <nalfaro@cfhp.com>
2024-04-30 10:01:27 -0500

    cleared template

--- src/App.js
+++ src/App.js
@@ -2,39 +2,22 @@ import React from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
   Grid,
   theme,
+  SimpleGrid,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <SimpleGrid minH="100%" columns={2} spacing={10}>
+          <Box bg='aqua' height='80px'></Box>
+          <Box bg='gold' height='80px'></Box>
+        </SimpleGrid>
+      </Grid>
     </ChakraProvider>
   );
 }
